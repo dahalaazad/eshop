0a47bf34de02e12cb7b65678da9edcb70d3681b1
Yubin Karki <yubin.karki@gurzu.net>
2022-11-22 13:13:48 +0545

    feat: Add forward button empty component

---
+++ src/screens/onboarding/components/ForwardButton.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function ForwardButton() {
+  return (
+    <View>
+      <Text>ForwardButton</Text>
+    </View>
+  );
+}
