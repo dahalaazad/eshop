569dc79f23d65c8ad5e09942b3bcf8310cb232a5
Aazad Dahal <aazad.dahal@gurzu.net>
2022-11-23 11:37:52 +0545

    Setup index for onboarding screens

---
+++ src/screens/onboarding/onboarding-screens/index.js
@@ -0,0 +1,9 @@
+import OnboardingScreenFirst from "./OnboardingScreenFirst";
+import OnboardingScreenSecond from './OnboardingScreenSecond'
+import OnboardingScreenThird from './OnboardingScreenThird'
+
+export {
+    OnboardingScreenFirst,
+    OnboardingScreenSecond,
+    OnboardingScreenThird
+}
