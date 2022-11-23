aa0e4ee41c263773f984e01139474dc7b3d6e481
Yubin Karki <yubin.karki@gurzu.net>
2022-11-23 21:09:54 +0545

    Update colors

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -1,1 +1,11 @@
-export const Colors = {};
+export const Colors = {
+  whiteColor: '#FFFFFF',
+  blackColor: '#000000',
+  loginBackgroundColor: '##F5F5F5',
+  mainBackgroundColor: '##FAFAFF',
+  subtitleTextColor: '#8F93AF',
+  footerTextColor: '#635555',
+  textLinkColor: '#212E7E',
+  inputFieldIconColor: '#868686',
+  buttonShadowColor: '#212E7E',
+};
