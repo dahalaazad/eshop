e3b7e5e882b3838d1bfd2512d2ba518f0025d072
Aazad Dahal <aazad.dahal@gurzu.net>
2022-11-23 11:38:34 +0545

    Setup navigation for onboarding screens

--- src/routes/OnboardingStack.js
+++ src/routes/OnboardingStack.js
@@ -1,11 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { OnboardingScreenFirst, OnboardingScreenSecond, OnboardingScreenThird } from '../screens/onboarding/onboarding-screens/index';
+
+const OnboardingStackScreen = createStackNavigator();
 
 const OnboardingStack = () => {
   return (
-    <View>
-      <Text>OnboardingStack</Text>
-    </View>
+   <OnboardingStackScreen.Navigator name='OnboardingFirst'>
+    <OnboardingStackScreen.Screen name='OnboardingScreenFirst' component={OnboardingScreenFirst}/>
+    <OnboardingStackScreen.Screen name='OnboardingScreenSecond' component={OnboardingScreenSecond}/>
+    <OnboardingStackScreen.Screen name='OnboardingScreenThird' component={OnboardingScreenThird}/>
+   </OnboardingStackScreen.Navigator>
   )
 }
 
