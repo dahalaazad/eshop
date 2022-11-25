ed183fce2fb8289ce0aff2fc45a339ae56133d39
Yubin Karki <yubin.karki@gurzu.net>
2022-11-25 16:05:10 +0545

    feat: Add status bar background color

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {Text} from 'react-native';
+import {StatusBar} from 'react-native';
 import {AuthStack, MainStack} from '@app/routes';
+import {Colors} from '@app/constants';
 
 const App = () => {
   return (
     <NavigationContainer>
       {/* THERE CANNOT BE TWO STACKS HERE */}
       <AuthStack />
+      <StatusBar backgroundColor={Colors.loginBackgroundColor} />
     </NavigationContainer>
   );
 };
