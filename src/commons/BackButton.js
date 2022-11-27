3f09384522ec875a0a99513c6c5d7a4ad230fcda
Aazad Dahal <aazad.dahal@gurzu.net>
2022-11-27 22:45:10 +0545

    feat: Complete button- back and favourite

--- src/commons/BackButton.js
+++ src/commons/BackButton.js
@@ -1,10 +1,31 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import React from 'react';
+import AD from 'react-native-vector-icons/AntDesign';
+import {Colors} from '@app/constants';
 
-export default function BackButton() {
+export default function BackButton({navigation}) {
   return (
     <View>
-      <Text>BackButton</Text>
+      <TouchableOpacity
+        style={[styles.backButton,styles.shadowProp]}
+        onPress={() => alert('Back Button')}>
+        <AD name="left" size={25} color={Colors.backButtonBlue} />
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  backButton: {
+    backgroundColor: Colors.whiteColor,
+    borderRadius: 8,
+    padding: 12,
+  },
+  shadowProp: {
+    shadowColor: '#000',
+    shadowOffset: {width: -2, height: 8},
+    shadowOpacity:0.12,
+    shadowRadius: 3,
+  },
+  
+});
--- src/commons/FavoriteButton.js
+++ src/commons/FavoriteButton.js
@@ -1,10 +1,27 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, StyleSheet,TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import AD from 'react-native-vector-icons/AntDesign';
+import { Colors } from '@app/constants';
 
 export default function FavoriteButton() {
+  const [iconName, setIconName] = useState('hearto');
   return (
     <View>
-      <Text>FavoriteButton</Text>
+      <TouchableOpacity
+        style={styles.backButton}
+        onPress={() => {
+          iconName==='hearto'? setIconName('heart'): setIconName('hearto')
+        }}>
+        <AD name={iconName} size={25} color='red'/>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  backButton: {
+    backgroundColor: Colors.whiteColor,
+    borderRadius: 8,
+    padding: 12,
+  },
+});
