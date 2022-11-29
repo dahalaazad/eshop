c5538d7e8a7f2d9e3d80276f157691f08a1b2ab6
Aazad Dahal <aazad.dahal@gurzu.net>
2022-11-29 12:32:58 +0545

    feat: Add icons to Bottom Tab

--- src/routes/mainstack/BottomTabNav.js
+++ src/routes/mainstack/BottomTabNav.js
@@ -5,6 +5,10 @@ import {ShoppingCart} from '@app/screens';
 import {UserAccount} from '@app/screens';
 import {Colors} from '@app/constants';
 import {DashboardStack, ShopStack, ProfileStack} from '.';
+import HomeIcon from '@app/assets/svg/HomeIcon.svg';
+import ShopIcon from '@app/assets/svg/ShopIcon.svg';
+import CartIcon from '@app/assets/svg/CartIcon.svg';
+import ProfileIcon from '@app/assets/svg/ProfileIcon.svg';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,8 +16,40 @@ const BottomTabNav = () => {
   return (
     <Tab.Navigator
       screenOptions={({route}) => ({
-        tabBarActiveTintColor: Colors.textLinkColor,
-        tabBarInactiveTintColor: 'gray',
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          if (route.name === 'DashboardStack') {
+            iconName = focused ? (
+              <HomeIcon style={{color: Colors.iconBlueColor}} />
+            ) : (
+              <HomeIcon style={{color: Colors.inactiveIconGrayColor}} />
+            );
+          } else if (route.name === 'Shop') {
+            iconName = focused ? (
+              <ShopIcon style={{color: Colors.iconBlueColor}} />
+            ) : (
+              <ShopIcon style={{color: Colors.inactiveIconGrayColor}} />
+            );
+          } else if (route.name === 'Cart') {
+            iconName = focused ? (
+              <CartIcon style={{color: Colors.iconBlueColor}} />
+            ) : (
+              <CartIcon style={{color: Colors.inactiveIconGrayColor}} />
+            );
+          } else if (route.name === 'Account') {
+            iconName = focused ? (
+              <ProfileIcon style={{color: Colors.iconBlueColor}} />
+            ) : (
+              <ProfileIcon style={{color: Colors.inactiveIconGrayColor}} />
+            );
+          }
+
+          // You can return any component that you like here!
+          return iconName;
+        },
+
+        tabBarActiveTintColor: Colors.iconBlueColor,
+        tabBarInactiveTintColor: Colors.inactiveIconGrayColor,
         headerTitleAlign: 'center',
         headerStyle: {
           backgroundColor: Colors.mainBackgroundColor,
@@ -36,19 +72,25 @@ const BottomTabNav = () => {
       <Tab.Screen
         name="Shop"
         component={ShopStack}
-        options={{headerTitle: 'All Product'}}
+        options={{
+          headerTitle: 'All Product',
+        }}
       />
 
       <Tab.Screen
         name="Cart"
         component={ShoppingCart}
-        options={{headerTitle: 'Shopping Cart'}}
+        options={{
+          headerTitle: 'Shopping Cart',
+        }}
       />
 
       <Tab.Screen
         name="Account"
         component={ProfileStack}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+        }}
       />
     </Tab.Navigator>
   );
