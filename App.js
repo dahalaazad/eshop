de9b6f7fe9c8bde7b8c52da303a3da6b1c8f8d16
Yubin Karki <yubin.karki@gurzu.net>
2022-11-30 12:07:29 +0545

    Wrap with GestureHandler for carousel scroll

--- App.js
+++ App.js
@@ -3,13 +3,16 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import {AuthStack, MainStack} from '@app/routes';
 import {Colors} from '@app/constants';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const App = () => {
   return (
     <NavigationContainer>
-      {/* THERE CANNOT BE TWO STACKS HERE */}
-      <AuthStack />
-      <StatusBar backgroundColor={Colors.loginBackgroundColor} />
+      <GestureHandlerRootView style={{flex: 1}}>
+        {/* THERE CANNOT BE TWO STACKS HERE */}
+        <MainStack />
+        <StatusBar backgroundColor={Colors.loginBackgroundColor} />
+      </GestureHandlerRootView>
     </NavigationContainer>
   );
 };
