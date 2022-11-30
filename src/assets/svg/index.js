5bbd7860b8645859228ccb2e7d68253b83fb83d6
Aazad Dahal <aazad.dahal@gurzu.net>
2022-11-30 23:30:12 +0545

    fix: Index for svg icons

---
+++ src/assets/svg/index.js
@@ -0,0 +1,8 @@
+import CartIcon from './CartIcon.svg';
+import HomeIcon from './HomeIcon.svg';
+import MainLogo from './MainLogo.svg';
+import ProfileIcon from './ProfileIcon.svg';
+import ShopIcon from './ShopIcon.svg';
+import waveBackground from './waveBackground.svg';
+
+export {CartIcon, HomeIcon, MainLogo, ProfileIcon, ShopIcon, waveBackground};
