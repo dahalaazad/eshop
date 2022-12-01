3251f843d971da80b25f0d32adb0afb92f255ad5
Yubin Karki <yubin.karki@gurzu.net>
2022-12-01 13:21:52 +0545

    Separate data file for shop page

--- src/screens/shop/Shop.js
+++ src/screens/shop/Shop.js
@@ -3,12 +3,7 @@ import React from 'react';
 import {Colors} from '@app/constants';
 import {Styles} from '@app/screens/shop/ShopStyles';
 import {FilterButton, ProductCard, SearchBar} from '@app/commons';
-import HondaGearBoxImage1 from '@app/assets/svg/HondaGearBox1.svg';
-import HondaGearBoxImage2 from '@app/assets/svg/HondaGearBox2.svg';
-import HondaGearBoxImage3 from '@app/assets/svg/HondaGearBox3.svg';
-import HondaGearBoxImage4 from '@app/assets/svg/HondaGearBox4.svg';
-import EngineFilterImage from '@app/assets/svg/EngineFilter.svg';
-import BrakePadImage from '@app/assets/svg/BrakePad.svg';
+import {ProductCardData} from '@app/constants';
 
 export default function Shop({navigation}) {
   const onProductCardPress = itemId => {
@@ -42,7 +37,7 @@ export default function Shop({navigation}) {
           numColumns={2}
           columnWrapperStyle={{justifyContent: 'space-around'}}
           contentContainerStyle={{paddingTop: 20, paddingBottom: 80}}
-          data={productCardData}
+          data={ProductCardData}
           keyExtractor={data => data.id}
           renderItem={productListRender}
         />
@@ -50,62 +45,3 @@ export default function Shop({navigation}) {
     </View>
   );
 }
-
-const productCardData = [
-  {
-    id: 1,
-    productCardImage: <HondaGearBoxImage1 />,
-    productCardTitle: 'Honda Gear Box',
-    productCardSubTitle: 'Lorem ipsum',
-    productCardPrice: 1999,
-  },
-  {
-    id: 2,
-    productCardImage: <HondaGearBoxImage2 />,
-    productCardTitle: 'Brake Pad',
-    productCardSubTitle: 'Lorem ipsum',
-    productCardPrice: 1999,
-  },
-  {
-    id: 3,
-    productCardImage: <HondaGearBoxImage3 />,
-    productCardTitle: 'Engine Filter',
-    productCardSubTitle: 'Lorem ipsum',
-    productCardPrice: 1999,
-  },
-  {
-    id: 4,
-    productCardImage: <HondaGearBoxImage4 />,
-    productCardTitle: 'Brake Pad',
-    productCardSubTitle: 'Lorem ipsum',
-    productCardPrice: 1999,
-  },
-  {
-    id: 5,
-    productCardImage: <EngineFilterImage />,
-    productCardTitle: 'Engine Filter',
-    productCardSubTitle: 'Lorem ipsum',
-    productCardPrice: 1999,
-  },
-  {
-    id: 6,
-    productCardImage: <BrakePadImage />,
-    productCardTitle: 'Brake Pad',
-    productCardSubTitle: 'Lorem ipsum',
-    productCardPrice: 1999,
-  },
-  {
-    id: 7,
-    productCardImage: <EngineFilterImage />,
-    productCardTitle: 'Engine Filter',
-    productCardSubTitle: 'Lorem ipsum',
-    productCardPrice: 1999,
-  },
-  {
-    id: 8,
-    productCardImage: <BrakePadImage />,
-    productCardTitle: 'Brake Pad',
-    productCardSubTitle: 'Lorem ipsum',
-    productCardPrice: 1999,
-  },
-];
