8a3ece0872b1c9344a501669ee44d1fbe05d5a3a
Aazad Dahal <aazad.dahal@gurzu.net>
2022-12-01 12:38:42 +0545

    fix: Change svg icon name to PascalCase

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -3,6 +3,6 @@ import HomeIcon from './HomeIcon.svg';
 import MainLogo from './MainLogo.svg';
 import ProfileIcon from './ProfileIcon.svg';
 import ShopIcon from './ShopIcon.svg';
-import waveBackground from './waveBackground.svg';
+import WaveBackground from './WaveBackground.svg';
 
-export {CartIcon, HomeIcon, MainLogo, ProfileIcon, ShopIcon, waveBackground};
+export {CartIcon, HomeIcon, MainLogo, ProfileIcon, ShopIcon, WaveBackground};
