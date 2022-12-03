5307332fcf53b6e3657a36361b2ada41ddec080a
Yubin Karki <yubin.karki@gurzu.net>
2022-12-03 22:26:26 +0545

    feat: Cart page item component design

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 import {Colors} from './Colors';
 import {Images} from './Images';
 import ProductCardData from './ProductCardData';
+import ShoppingCartData from './ShoppingCartData';
 
-export {Colors, Images, ProductCardData};
+export {Colors, Images, ProductCardData, ShoppingCartData};
--- src/screens/cart/components/CartItem.js
+++ src/screens/cart/components/CartItem.js
@@ -1,10 +1,38 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
+import {Styles} from '@app/screens/cart/components/CartItemStyles';
+import CartItemImage from '@app/screens/cart/components/CartItemImage';
+import BrakePadImage from '@app/assets/svg/BrakePad.svg';
+import EngineFilterImage from '@app/assets/svg/EngineFilter.svg';
+import HondaGearBox1Image from '@app/assets/svg/HondaGearBox1.svg';
+import CrossImage from '@app/assets/svg/CrossImage.svg';
+import {ProductCounter} from '@app/commons';
 
-export default function CartItem() {
+export default function CartItem({svgImage, titleText, subTitleText}) {
+  console.log(titleText, subTitleText, 'in cartitem page');
+
   return (
-    <View>
-      <Text>CartItem</Text>
+    <View style={Styles.mainContainer}>
+      <View style={{flexDirection: 'row'}}>
+        <View style={Styles.leftImageContainer}>
+          <CartItemImage svgImage={svgImage} />
+        </View>
+        <View style={Styles.midTextContainer}>
+          <Text style={Styles.titleText}>{titleText}</Text>
+          <Text style={Styles.subTitleText}>{subTitleText}</Text>
+          <Text style={Styles.priceText}>{`Rs. ${'1999'}`}</Text>
+        </View>
+      </View>
+
+      <View style={Styles.rightButtonContainer}>
+        <TouchableOpacity style={Styles.crossButton}>
+          <CrossImage />
+        </TouchableOpacity>
+
+        <View style={Styles.counterButtonContainer}>
+          <ProductCounter />
+        </View>
+      </View>
     </View>
   );
 }
