c371e51a8fff22dc66b890c0c35d66124f561ef4
Aazad Dahal <aazad.dahal@gurzu.net>
2022-12-05 08:39:40 +0545

    fix: Require Cycle error

--- src/routes/authstack/AuthStack.js
+++ src/routes/authstack/AuthStack.js
@@ -1,8 +1,9 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {OnboardingStack, LoginStack} from '.';
 import { MainStack } from '@app/routes/mainstack';
+import OnboardingStack from './OnboardingStack';
+import LoginStack from './LoginStack';
 
 const Stack = createNativeStackNavigator();
 const AuthStack = () => {
--- src/routes/mainstack/index.js
+++ src/routes/mainstack/index.js
@@ -1,15 +1,5 @@
 import BottomTabNav from './BottomTabNav';
 import CartStack from './CartStack';
-import DashboardStack from './DashboardStack';
 import MainStack from './MainStack';
-import ProfileStack from './ProfileStack';
-import ShopStack from './ShopStack';
 
-export {
-  BottomTabNav,
-  CartStack,
-  DashboardStack,
-  MainStack,
-  ProfileStack,
-  ShopStack,
-};
+export {BottomTabNav, CartStack, MainStack};
