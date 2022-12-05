e3b87d54e95a9f2a89ecc7678800499fcba513b5
Yubin Karki <yubin.karki@gurzu.net>
2022-12-05 19:31:13 +0545

    Add colors and data

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,5 +2,6 @@ import {Colors} from './Colors';
 import {Images} from './Images';
 import ProductCardData from './ProductCardData';
 import ShoppingCartData from './ShoppingCartData';
+import MyCartData from './MyCartData';
 
-export {Colors, Images, ProductCardData, ShoppingCartData};
+export {Colors, Images, ProductCardData, ShoppingCartData, MyCartData};
