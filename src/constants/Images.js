f7feb8eba94813a50a1e0240fdcadf435342b82e
Yubin Karki <yubin.karki@gurzu.net>
2022-12-07 10:22:24 +0000

    Feature/account

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -1,3 +1,6 @@
 export default {
   profileBeardedMan: require('../assets/images/UserProfileBeardedMan.png'),
+  profileWomanAccount: require('../assets/images/AccountProfileWoman.png'),
+  test: require('../assets/images/test.jpg'),
+  profileManImage: require('../assets/images/AccountProfileImage.png'),
 };
