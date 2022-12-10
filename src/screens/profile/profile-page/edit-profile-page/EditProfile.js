ae3df209f5cbe96ad47366ddcf2a9755761eccd1
Aazad Dahal <aazad.dahal@gurzu.net>
2022-12-10 20:44:59 +0545

    fix: Solve screen items going under the header

--- src/screens/profile/profile-page/edit-profile-page/EditProfile.js
+++ src/screens/profile/profile-page/edit-profile-page/EditProfile.js
@@ -1,8 +1,13 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import React, {useLayoutEffect} from 'react';
 import {BackButton} from '@app/commons';
+import {Colors} from '@app/constants';
+import Images from '@app/constants/Images';
+import {useHeaderHeight} from '@react-navigation/elements';
 
 export default function EditProfile({navigation}) {
+  const headerHeight = useHeaderHeight();
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerLeft: (...props) => (
@@ -11,11 +16,44 @@ export default function EditProfile({navigation}) {
     });
   }, [navigation]);
   return (
-    <View style={styles.container}>
-      <Text style={{color: '#000', fontSize: 30}}>EditProfile</Text>
+    <View style={[styles.container,{marginTop:headerHeight}]}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={Images.profileManImage}
+          style={styles.imageStyle}
+          resizeMode="cover"
+        />
+      </View>
+      <View></View>
+      <View></View>
+      <View></View>
+      <View></View>
+      <View></View>
     </View>
   );
 }
 const styles = StyleSheet.create({
-  container: {alignItems: 'center', justifyContent: 'center', flex: 1},
+  container: {
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    flex: 1,
+    backgroundColor: Colors.whiteColor,
+  },
+  imageContainer: {
+    height: 122,
+    width: 122,
+    overflow: 'hidden',
+    borderWidth: 2,
+    borderColor: 'white',
+    borderRadius: 80,
+    // alignSelf: 'center',
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+  },
+  imageStyle: {
+    width: '100%',
+    height: '100%',
+  },
 });
