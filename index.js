5d64496c158a5eeae0aac9fa74d9b132ff773338
aazad.dahal <aazad.dahal@gurzu.net>
2023-01-18 11:38:54 +0545

    feat: Finish redux-persist setup

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {Provider} from 'react-redux';
-import {store} from '@app/redux/store';
+import {persistor, store} from '@app/redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 function Root() {
   return (
     <Provider store={store}>
       <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   );
 }
