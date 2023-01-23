121f7e4af228097eab1a652c15ca607f9fdebe2f
aazad.dahal <aazad.dahal@gurzu.net>
2023-01-23 13:47:19 +0545

    feat: Check for firstLoad and isSignedIn

--- src/routes/authstack/AuthStack.js
+++ src/routes/authstack/AuthStack.js
@@ -1,19 +1,27 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { MainStack } from '@app/routes/mainstack';
-import OnboardingStack from './OnboardingStack';
-import LoginStack from './LoginStack';
+import {OnboardingScreen, SplashScreen, Login, Signup} from '@app/screens';
+import {MainStack} from '@app/routes/mainstack';
+import {useSelector} from 'react-redux';
 
 const Stack = createNativeStackNavigator();
 const AuthStack = () => {
+  const firstLoad = useSelector(state => state?.auth?.firstLoad);
+  const isLoggedIn = useSelector(state => state?.auth?.isLoggedIn);
+
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="OnboardingStack" component={OnboardingStack} />
+      <Stack.Screen name="SplashScreen" component={SplashScreen} />
+      {firstLoad ? (
+        <Stack.Group>
+          <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
 
-      <Stack.Screen name="LoginStack" component={LoginStack} />
-
-      <Stack.Screen name='MainStack' component={MainStack}/>
+          <Stack.Screen name="SignupPage" component={Signup} />
+        </Stack.Group>
+      ) : null}
+      <Stack.Screen name="LoginPage" component={Login} />
+      {isLoggedIn && <Stack.Screen name="MainStack" component={MainStack} />}
     </Stack.Navigator>
   );
 };
