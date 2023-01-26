bc21105d9a6aeabe6deb77370a7b33a119c83845
aazad.dahal <aazad.dahal@gurzu.net>
2023-01-26 11:37:13 +0545

    feat: Validation rules for phone and address

--- src/constants/InputRules.js
+++ src/constants/InputRules.js
@@ -36,4 +36,20 @@ export const InputRules = {
       message: 'Confirm your password',
     },
   },
+  phone: {
+    required: {
+      value: true,
+      message: 'Provide enter phone number',
+    },
+    pattern: {
+      value: /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/,
+      message: 'Enter a valid phone number',
+    },
+  },
+  address: {
+    required: {
+      value: true,
+      message: 'Provide enter your address',
+    },
+  },
 };
